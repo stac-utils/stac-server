abe9d8b9aed0f20e9d5b81d0e459a9379bf9c143
Matthew Hanson <matt.a.hanson@gmail.com>
2019-12-19 14:03:06 -0500

    updated ingest lambda

--- lambdas/ingest/index.js
+++ lambdas/ingest/index.js
@@ -5,40 +5,14 @@ const logger = console
 
 
 module.exports.handler = async function handler(event) {
-  logger.info(`Ingest Event: ${JSON.stringify(event)}`)
+  logger.info(`Event: ${JSON.stringify(event)}`)
   try {
-    if (event.Records && (event.Records[0].EventSource === 'aws:sns')) {
-      // event is SNS message of updated file on s3
-      const message = JSON.parse(event.Records[0].Sns.Message)
-      if (message.type && message.type === 'Feature') {
-        // event is a STAC Item
-        await satlib.ingest.ingestItem(message, satlib.es)
-      } else {
-        // updated s3
-        const { Records: s3Records } = message
-        const promises = s3Records.map((s3Record) => {
-          const {
-            s3: {
-              bucket: { name: bucketName },
-              object: { key }
-            }
-          } = s3Record
-          const url = `https://${bucketName}.s3.amazonaws.com/${key}`
-          logger.log(`Ingesting catalog file ${url}`)
-          const recursive = false
-          return satlib.ingest.ingest(url, satlib.es, recursive)
-        })
-        await Promise.all(promises)
-      }
-    } else if ((event.type && event.type === 'Feature') || (event.id && event.extent)) {
-      // event is STAC Item or Collection JSON
-      await satlib.ingest.ingestItem(event, satlib.es)
-    } else if (event.url) {
-      // event is URL to a catalog node
-      const { url, recursive, collectionsOnly } = event
-      const recurse = recursive === undefined ? true : recursive
-      const collections = collectionsOnly === undefined ? false : collectionsOnly
-      await satlib.ingest.ingest(url, satlib.es, recurse, collections)
+    if (event.Records) {
+      const records = await Promise.all(event.Records.map((record) => {
+        const item = JSON.parse(record.body)
+        satlib.ingest(item, satlib.es)
+        logger.log(`Ingested ${item.id}`)
+      }))
     }
   } catch (error) {
     console.log(error)
--- lambdas/queue/index.js
+++ lambdas/queue/index.js
@@ -1,7 +1,20 @@
 'use strict'
 
+const AWS = require('aws-sdk')
 const logger = console
 
+// Create an SQS service object
+const sqs = new AWS.SQS({ apiVersion: '2012-11-05' })
+
+async function getQueueUrl() {
+  return sqs.getQueueUrl({ QueueName: process.env.queueName }, (err, data) => {
+    if (err) {
+      logger.error(`Error: ${err}`)
+      throw err
+    }
+    return data.QueueUrl
+  })
+}
 
 module.exports.handler = async function handler(event) {
   logger.debug(`Event: ${JSON.stringify(event)}`)
@@ -12,13 +25,24 @@ module.exports.handler = async function handler(event) {
       // event is SNS message
       item = JSON.parse(event.Records[0].Sns.Message)
     }
+    logger.debug(`Item: ${JSON.stringify(item)}`)
 
     // Is Item OR Collection
     if ((item.type && item.type === 'Feature') || (item.id && item.extent)) {
-      // TODO - add to queue
+      // get queue URL from ARN
+      let resp = await sqs.getQueueUrl({ QueueName: process.env.queueName }).promise()
+
+      const queue_url = resp.QueueUrl
+      // add to queue
+      const params = {
+        MessageBody: JSON.stringify(item),
+        QueueUrl: queue_url
+      }
+      resp = await sqs.sendMessage(params).promise()
+      logger.info(`Added to queue ${queue_url}`)
     }
   } catch (error) {
-    console.log(error)
+    logger.log(error)
   }
 }
 

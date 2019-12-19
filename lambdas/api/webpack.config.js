8a2115d6a5f39a09bfe6dcd5e77aa7d0afa69073
Matthew Hanson <matt.a.hanson@gmail.com>
2019-12-19 02:09:19 -0500

    reorg cleanup

--- lambdas/api/webpack.config.js
+++ lambdas/api/webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
   plugins: [
     new CopyPlugin([
       {
-        from: '../api-lib/api.yaml',
+        from: '../../libs/api.yaml',
         to: 'api.yaml'
       }
     ]),
---
+++ libs/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable global-require */
+
+'use strict'
+
+module.exports = {
+  api: require('./api.js'),
+  es: require('./es.js'),
+  ingest: require('./ingest.js'),
+  logger: require('./logger.js')
+}

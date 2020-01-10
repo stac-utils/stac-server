62ad2c2a084947f07dce0dba3e77e51ef598521e
marquet <marquet@element84.com>
2020-01-10 11:59:29 -0500

    working on fixing github actions

--- .github/fast-tests.sh
+++ .github/fast-tests.sh
@@ -5,7 +5,8 @@ apt-get install sudo
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 # sudo apt update
-sudo apt install yarn -y
-yarn --version
+# sudo apt install yarn -y
+sudo apt install npm -y
+# yarn --version
 # yarn run test
 npm run test

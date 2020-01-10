8cd6db572bdb8b023bc7c9a55bc4ee67d6079afc
Drew Daniel <drew@element84.com>
2020-01-10 13:15:52 -0500

    trying to get github actions to work

--- .github/fast-tests.sh
+++ .github/fast-tests.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 echo "Running test 📝"
-apt-get update
-apt-get install sudo
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+#apt-get update
+#apt-get install sudo
+#curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+#echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 # sudo apt update
 # sudo apt install yarn -y
-sudo apt install npm -y
+# sudo apt install npm -y
 # yarn --version
 # yarn run test
-npm run test
+# npm run test

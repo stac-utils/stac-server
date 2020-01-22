72dfd81f362d99f03a4dede2c617095bf11a91cc
marquet <marquet@element84.com>
2020-01-22 12:45:22 -0500

    Removed console.logs that were not needed

--- libs/es.js
+++ libs/es.js
@@ -354,10 +354,42 @@ function buildIdsQuery(ids) {
   }
 }
 
+// function buildSort(parameters) {
+//   const { sort } = parameters
+//   let sorting
+//   if (sort && sort.length > 0) {
+//     sorting = sort.map((sortRule) => {
+//       const { field, direction } = sortRule
+//       const propertyKey = `properties.${field}`
+//       return {
+//         [propertyKey]: {
+//           order: direction
+//         }
+//       }
+//     })
+//   } else {
+//     // Default item sorting
+//     sorting = [
+//       { 'properties.datetime': { order: 'desc' } }
+//     ]
+//   }
+//   return sorting
+// }
+
 function buildSort(parameters) {
-  const { sort } = parameters
+  const { sort, sortBy } = parameters
   let sorting
-  if (sort && sort.length > 0) {
+  if (sortBy && sortBy.length > 0) {
+    sorting = sortBy.map((sortRule) => {
+      const { field, direction } = sortRule
+      const propertyKey = `properties.${field}`
+      return {
+        [propertyKey]: {
+          order: direction
+        }
+      }
+    })
+  } else if (sort && sort.length > 0) {
     sorting = sort.map((sortRule) => {
       const { field, direction } = sortRule
       const propertyKey = `properties.${field}`

195ff0e8c3229d3c0c6910acd6218f0e58276305
Matthew Hanson <matt.a.hanson@gmail.com>
2020-01-30 19:18:26 -0500

    fix index creation

--- libs/es.js
+++ libs/es.js
@@ -86,8 +86,8 @@ async function create_indices() {
   const indexExists = await client.indices.exists('collections')
   if (!indexExists) {
     try {
-      await client.indices.create(collections_mapping)
-      await client.indices.create(items_mapping)
+      await client.indices.create({ index: 'collections', body: collections_mapping })
+      await client.indices.create({ index: 'items', body: items_mapping })
       logger.info('Created indices')
     } catch (error) {
       const debugMessage = `Error creating index, already created: ${error}`

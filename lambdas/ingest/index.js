556e6d6b316c37a42fb2ea27c85d8b860395f3ff
Matthew Hanson <matt.a.hanson@gmail.com>
2020-03-09 23:18:10 -0400

    sns straight to queue

--- lambdas/ingest/index.js
+++ lambdas/ingest/index.js
@@ -9,20 +9,28 @@ module.exports.handler = async function handler(event) {
   if (event.create_indices) {
     await satlib.es.create_indices()
   }
+
+  // start with message as is
+  let items = [event]
+  // if this is SQS
+  if (event.Records) {
+    items = await Promise.all(event.Records.map(async (record) => {
+      let i = JSON.parse(record.body)
+      // if event record is an SNS notification
+      if (i.Type === 'Notification') {
+        i = JSON.parse(i.Message)
+      }
+      logger.debug(`Record: ${JSON.stringify(i)}`)
+      return i
+    }))
+  }
+
   try {
-    if (event.Records) {
-      logger.info(`Ingesting ${event.Records.length} items`)
-      const items = await Promise.all(event.Records.map(async (record) => {
-        const item = JSON.parse(record.body)
-        logger.debug(`Item: ${JSON.stringify(item)}`)
-        return item
-      }))
-      await satlib.ingest.ingestItems([item], satlib.es)
-      logger.debug(`${items.length} Items: ${JSON.stringify(items)}`)
-    }
+    logger.info(`Ingesting ${items.length} items`)
+    await satlib.ingest.ingestItems(items, satlib.es)
+    logger.debug(`Ingested ${items.length} Items: ${JSON.stringify(items)}`)
   } catch (error) {
     console.log(error)
     throw(error)
   }
 }
-

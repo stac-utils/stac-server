78ca1c00be9097b70c0e5ddd2f634c4848d048e6
Matthew Hanson <matt.a.hanson@gmail.com>
2020-03-20 23:59:56 -0400

    log each failed record

--- lambdas/failed-ingest/index.js
+++ lambdas/failed-ingest/index.js
@@ -1,9 +1,16 @@
 'use strict'
 
-const AWS = require('aws-sdk')
 const logger = console
 
 module.exports.handler = async function handler(event) {
-  logger.debug(`Failed ingest: ${JSON.stringify(event)}`)
+  logger.info(`Failed ingest: ${JSON.stringify(event)}`)
+  // log each incoming SNS message
+  if (event.Records) {
+    let msg
+    event.Records.forEach((record) => {
+      msg = JSON.parse(JSON.parse(record.body).Message)
+      logger.info(JSON.stringify(msg))
+    })
+  }
 }
 

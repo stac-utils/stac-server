28de83fdb06b99c9433e6551a9e79c854640d433
Marc Huffnagle <marc@element84.com>
2022-02-19 22:25:08 -0500

    Additional testing

--- bin/integration-tests.sh
+++ bin/integration-tests.sh
@@ -5,7 +5,15 @@ set -e
 ./bin/wait-for-elasticsearch/run.sh
 
 echo "Setting up Elasticsearch"
-node tests/integration/setup.js
+node ./tests/integration/setup-es.js
+
+echo "Configuring serverless-offline"
+if [ -e serverless.yml ]; then
+  mv serverless.yml serverless.yml.original
+fi
+
+grep -v 'ElasticSearchInstance, DomainEndpoint' serverless.yml.example |\
+  sed 's/ES_HOST.*/ES_HOST: http:\/\/localhost:9200/' > serverless.yml
 
 echo "Starting serverless-offline"
 npx serverless offline start >/dev/null 2>&2 &
@@ -21,5 +29,9 @@ set -e
 
 echo "Stopping serverless-offline"
 kill "$SERVERLESS_PID"
+
+if [ -e serverless.yml.original ]; then
+  mv serverless.yml.original serverless.yml
+fi
 
 exit "$TEST_RESULT"

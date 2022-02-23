4b7c0cdad2cb1dd9d68dd4d542a9f06a9b21f2cb
Marc Huffnagle <marc@element84.com>
2022-02-23 11:46:36 -0500

    Add createCollectionsIndex helper

--- tests/helpers/es.js
+++ tests/helpers/es.js
@@ -1,12 +1,23 @@
 // @ts-check
 
-const { connect } = require('../../src/lib/esClient')
+const { connect, createIndex } = require('../../src/lib/esClient')
 
+/**
+ * @returns {Promise<void>}
+ */
+const createCollectionsIndex = async () => {
+  await createIndex('collections')
+}
+
+/**
+ * @returns {Promise<void>}
+ */
 const refreshIndices = async () => {
   const esClient = await connect()
   await esClient.indices.refresh({ index: '_all' })
 }
 
 module.exports = {
+  createCollectionsIndex,
   refreshIndices
 }

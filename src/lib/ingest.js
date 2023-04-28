d134fe689804cbe4ab42746939ee645ff086e959
Justin Walgran <jwalgran@azavea.com>
2023-04-28 12:09:41 -0700

    Remove separate ingestItem implementation

--- src/lib/ingest.js
+++ src/lib/ingest.js
@@ -2,30 +2,6 @@ import { Readable } from 'readable-stream'
 import pump from 'pump'
 import logger from './logger.js'
 
-export async function ingestItem(item, stream) {
-  const readable = new Readable({ objectMode: true })
-  const { toDB, dbStream } = await stream()
-  const promise = new Promise((resolve, reject) => {
-    pump(
-      readable,
-      toDB,
-      dbStream,
-      (error) => {
-        if (error) {
-          logger.error('Error ingesting', error)
-          reject(error)
-        } else {
-          logger.info(`Ingested item ${item.id}`)
-          resolve(true)
-        }
-      }
-    )
-  })
-  readable.push(item)
-  readable.push(null)
-  return promise
-}
-
 export async function ingestItems(items, stream) {
   const readable = new Readable({ objectMode: true })
   const { toDB, dbStream } = await stream()
@@ -48,4 +24,8 @@ export async function ingestItems(items, stream) {
   items.forEach((item) => readable.push(item))
   readable.push(null)
   return promise
+}
+
+export async function ingestItem(item, stream) {
+  return ingestItems([item], stream)
 }
